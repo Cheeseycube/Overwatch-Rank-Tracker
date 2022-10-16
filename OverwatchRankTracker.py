e393a2a0d6058c0448344e6dc55131a99e5ea0f1
Joseph Rousoss <jroussos01@gmail.com>
2022-10-15 22:50:58 -0400

    added tank specific wins/losses

--- OverwatchRankTracker.py
+++ OverwatchRankTracker.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 import PySimpleGUI as sg
 import sys
 
-
-
+gameOutcome = "nan"
+Role = "nan"
 # new plan: refactor my columns to be more categorical:  each row is one game with the following columns: win/loss/draw, role, map, gameMode, etc...
 # also there will be a column for the day's date, or maybe just a column that is how many days in that particular observation is, starting at 1 for the first observation
 
@@ -17,7 +17,7 @@ import sys
 
 ''' FUNCTIONS '''
 
-def addrow(gameOutcome, Role):
+def addrow():
     try:
         #mydata.loc[len(mydata.index)] = [gameOutcome, Role]
         df = {"Win/Loss/Draw": gameOutcome, "Role": Role}
@@ -25,8 +25,12 @@ def addrow(gameOutcome, Role):
     except:
         print("cannot set a row with mismatached columns OR some other error idk")
         
-
-
+def createTankdata():
+    Tankdata = EncodedData
+    for i in EncodedData.index:
+        if (EncodedData["Role"][i] != "tank"):
+            Tankdata = Tankdata.drop([EncodedData.index[i]])
+    return Tankdata
 
 
 
@@ -56,7 +60,7 @@ if ('Role' not in mydata.columns):
 
 ''' GETTING USER INPUT '''
 
-layout = [  [sg.Text("Did you win or lose? (click the corresponding box)")], 
+layout = [  [sg.Text("Did you win or lose? (click the corresponding box)", font = ("Helvetica", 20) )], 
             [sg.Button("Win"), sg.Button("Loss"), sg.Exit()] ]
             
 
@@ -75,18 +79,53 @@ while True: # while the window is active do this
         print("win")
         #mydata.at[0, 'totalWon'] += 1   # just... no
         gameOutcome = "win"
-        mydata = addrow('win', 'nan')
         break
     if (event == 'Loss'):
         print("loss")
         #mydata.at[0, 'totalLost'] += 1  # please stop
         gameOutcome = "loss"
-        mydata = addrow('loss', 'nan')
         break
 
 # closes the window after the loop breaks
 InputWindow.close()
 
+
+
+
+layout = [  [sg.Text("What role did you play as?", font = ("Helvetica", 20) )], 
+            [sg.Button("Tank"), sg.Button("Support"), sg.Button("Damage"), sg.Exit()] ]
+            
+
+InputWindow = sg.Window('Overwatch Stats Program', layout, margins = (100, 100))    
+
+while True: # while the window is active do this
+    event, values = InputWindow.read()
+    if (event == sg.WIN_CLOSED):
+        break
+    if (event == 'Exit'):
+        InputWindow.close()
+        print("exited")
+        #sys.exit(0) 
+        break
+    if (event == 'Tank'):
+        print("tank")
+        Role = "tank"
+        break
+    if (event == 'Support'):
+        print("Support")
+        Role = "support"
+        break
+    if (event == 'Damage'):
+        print("Damage")
+        Role = "damage"
+        break
+
+# closes the window after the loop breaks
+InputWindow.close()
+
+mydata = addrow()   # this could be problematic if we just want to view statistics
+
+# to curb the above problem, there should be a button that brings up stats so you can see them without making changes
 
 
 ''' CLEANING THE DATA '''
@@ -105,19 +144,40 @@ mydata = mydata[ ['Win/Loss/Draw', 'Role'] ]
 
 # One-hot encoding win/loss/draw 
 EncodedData = pd.get_dummies(mydata, columns =["Win/Loss/Draw"], prefix = ["Outcome"])
+Tankdata = createTankdata()
 
-try:
+# could use 'if outcomewin in columns' instead of try except I think?
+
+    
+if ('Outcome_win' in EncodedData.columns):
     Wins = EncodedData["Outcome_win"].sum()
-except:
+else:
     Wins = 0
     
-try:
+if ('Outcome_loss' in EncodedData.columns):
     Losses = EncodedData["Outcome_loss"].sum()
-except:
+else:
     Losses = 0
 
-layout = [  [sg.Text("Total games won this season: " + str(Wins) )], 
-            [sg.Text("Total games lost this season: " + str(Losses) )], 
+# if statements may be unneccessary here
+if ('Outcome_win' in Tankdata.columns):
+    Tank_Wins = Tankdata["Outcome_win"].sum()
+else:
+    Tank_Wins = 0
+    
+if ('Outcome_loss' in Tankdata.columns):
+    Tank_Losses = Tankdata["Outcome_loss"].sum()
+else:
+    Tank_Losses = 0
+
+
+
+# could use f-strings here instead of concatenation?
+
+layout = [  [sg.Text("Total games won this season: " + str(Wins), font = ("Helvetica", 20) )], # trying font stuff
+            [sg.Text("Total games lost this season: " + str(Losses), font = ("Helvetica", 20) )],
+            [sg.Text("Total games won on tank this season: " + str(Tank_Wins), font = ("Helvetica", 20) )],
+            [sg.Text("Total games lost on tank this season: " + str(Tank_Losses), font = ("Helvetica", 20) )],
             [sg.Button('Close')]]
 
 statsWindow = sg.Window('Stats Window', layout, margins = (100, 100))
